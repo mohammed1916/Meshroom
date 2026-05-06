73e9a91a1a977a01de60e0e78aaa0ef377f641a7
Mohammed Abdullah <mohammed.a@bigbangboom.com>
2026-05-06 09:39:47 +0530

    paths in start_m

---
+++ start_m.bat
@@ -0,0 +1,17 @@
+REM Windows
+REM Add the aliceVision and qtPlugins folders with the binaries to this directory
+
+set MESHROOM_INSTALL_DIR=%CD%
+set PYTHONPATH=%CD%
+
+REM # Development options
+REM set MESHROOM_OUTPUT_QML_WARNINGS=1
+REM set MESHROOM_INSTANT_CODING=1
+REM set QT_PLUGIN_PATH=C:\dev\meshroom\install
+REM set QML2_IMPORT_PATH=C:\dev\meshroom\install\qml
+REM set ALICEVISION_ROOT=C:\Users\BBBS-AI-01\d\Meshroom\aliceVision\install
+REM set ALICEVISION_LIBPATH=%ALICEVISION_ROOT%\bin;C:\dev\vcpkg\installed\x64-windows\bin
+
+REM PYTHONPATH=%ALICEVISION_ROOT%\lib\python;%PYTHONPATH%
+
+python meshroom\ui
